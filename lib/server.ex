81301d7d75ba50506a7e7c8d3b1a66af4a439443
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-07-27 16:56:37 +0530

    17-exercise-3

--- lib/server.ex
+++ lib/server.ex
@@ -163,7 +163,10 @@ require Logger
   end
 
   defp empty_rdb_file do
-    Base.decode64!("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
+    content = Base.decode64!("UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==")
+    IO.puts("RDB file size: #{byte_size(content)} bytes")
+    IO.puts("RDB file header: #{inspect(binary_part(content, 0, 9))}")
+    content
   end
 
   defp execute_command_with_config(command, args, client, config) do
