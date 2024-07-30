d9c8cb1fc59310258e559ce4812ad42b65e9ecc0
ProgMastermind <surya.21mis7126@vitapstudent.ac.in>
2024-07-30 22:46:56 +0530

    debug-38

--- lib/server.ex
+++ lib/server.ex
@@ -60,7 +60,7 @@ require Logger
           :ok ->
             IO.puts("Handshake completed successfully")
             # spawn(fn -> listen_for_master_commands(socket) end)
-            listen_for_master_commands(socket)
+            # listen_for_master_commands(socket)
             # {:ok, socket}
           {:error, reason} ->
             IO.puts("Handshake failed: #{inspect(reason)}")
@@ -153,84 +153,6 @@ require Logger
     end
   end
 
-  defp listen_for_master_commands(socket) do
-    IO.puts("Listening for master commands...")
-    case receive_rdb_file(socket) do
-      :ok ->
-        IO.puts("RDB file received successfully")
-        process_master_commands(socket, "")
-      {:error, reason} ->
-        IO.puts("Error receiving RDB file: #{inspect(reason)}")
-    end
-  end
-
-  defp receive_rdb_file(socket) do
-    case :gen_tcp.recv(socket, 0, 5000) do
-      {:ok, "$" <> rest} ->
-        [length_str, rdb_data] = String.split(rest, "\r\n", parts: 2)
-        length = String.to_integer(length_str)
-        if byte_size(rdb_data) < length do
-          case :gen_tcp.recv(socket, length - byte_size(rdb_data), 5000) do
-            {:ok, remaining_data} ->
-              IO.puts("Received full RDB file")
-              :ok
-            {:error, :closed} ->
-              IO.puts("Connection closed while receiving RDB file")
-              {:error, :closed}
-            error -> error
-          end
-        else
-          IO.puts("Received full RDB file")
-          :ok
-        end
-      {:error, :closed} ->
-        IO.puts("Connection closed before receiving RDB file")
-        {:error, :closed}
-      error -> error
-    end
-  end
-
-  defp process_master_commands(socket, buffer) do
-    case :gen_tcp.recv(socket, 0) do
-      {:ok, data} ->
-        new_buffer = buffer <> data
-        case process_buffer(new_buffer) do
-          {:ok, rest} -> process_master_commands(socket, rest)
-          {:continuation, _} -> process_master_commands(socket, new_buffer)
-        end
-      {:error, :closed} ->
-        IO.puts("Connection to master closed")
-      {:error, reason} ->
-        IO.puts("Error receiving data from master: #{inspect(reason)}")
-    end
-  end
-
-  defp process_buffer(buffer) do
-    case Server.Protocol.parse(buffer) do
-      {:ok, command, rest} ->
-        execute_master_command(command)
-        {:ok, rest}
-      other -> other
-    end
-  end
-
-  defp execute_master_command(parsed_data) do
-    IO.puts("Executing master command: #{inspect(parsed_data)}")
-    case parsed_data do
-      ["SET", key, value | rest] ->
-        case rest do
-          ["PX", expire_time] ->
-            time_ms = String.to_integer(expire_time)
-            Server.Store.update(key, value, time_ms)
-          [] ->
-            Server.Store.update(key, value)
-        end
-      _ ->
-        IO.puts("Unknown command from master: #{inspect(parsed_data)}")
-    end
-  end
-
-
   #----------------------------------------------------------------------------------
 
   # Master Server Code
@@ -245,18 +167,18 @@ require Logger
   end
 
 
-  defp serve(client, config) do
-    try do
-      client
-      |> read_line()
-      |> process_command(client, config)
+  # defp serve(client, config) do
+  #   try do
+  #     client
+  #     |> read_line()
+  #     |> process_command(client, config)
 
-      serve(client, config)
-    catch
-      kind, reason ->
-        {:error, {kind, reason, __STACKTRACE__}}
-    end
-  end
+  #     serve(client, config)
+  #   catch
+  #     kind, reason ->
+  #       {:error, {kind, reason, __STACKTRACE__}}
+  #   end
+  # end
 
   defp read_line(client) do
     case :gen_tcp.recv(client, 0) do
@@ -265,24 +187,71 @@ require Logger
     end
   end
 
-  defp process_command(command, client, config) do
-    IO.puts("Received command: #{inspect(command)}")  # Debug line
-    case Server.Protocol.parse(command) do
-      {:ok, parsed_data, _rest} ->
-        IO.puts("Parsed data: #{inspect(parsed_data)}")  # Debug line
-        handle_command(parsed_data, client, config)
-      {:continuation, _fun} ->
-        IO.puts("Incomplete command")  # Debug line
-        write_line("-ERR Incomplete command\r\n", client)
+  # defp process_command(command, client, config) do
+  #   IO.puts("Received command: #{inspect(command)}")  # Debug line
+  #   case Server.Protocol.parse(command) do
+  #     {:ok, parsed_data, _rest} ->
+  #       IO.puts("Parsed data: #{inspect(parsed_data)}")  # Debug line
+  #       handle_command(parsed_data, client, config)
+  #     {:continuation, _fun} ->
+  #       IO.puts("Incomplete command")  # Debug line
+  #       write_line("-ERR Incomplete command\r\n", client)
+  #   end
+  # end
+
+  # defp handle_command(parsed_data, client, config) do
+  #   case parsed_data do
+  #     [command | args] ->
+  #       execute_command_with_config(String.upcase(to_string(command)), args, client, config)
+  #     _ ->
+  #       write_line("-ERR Invalid command format\r\n", client)
+  #   end
+  # end
+
+  defp serve(client, config) do
+    is_master = is_master_connection?(client, config)
+    serve_loop(client, config, is_master, "")
+  end
+
+  defp is_master_connection?(client, config) do
+    case :inet.peername(client) do
+      {:ok, {address, _port}} ->
+        address == elem(config.replica_of || {nil, nil}, 0)
+      _ -> false
     end
   end
 
-  defp handle_command(parsed_data, client, config) do
+  defp serve_loop(client, config, is_master, buffer) do
+    case read_line(client) do
+      {:ok, data} ->
+        new_buffer = buffer <> data
+        case Server.Protocol.parse_multi(new_buffer, 1) do
+          {:ok, [command], rest} ->
+            process_command(command, client, config, is_master)
+            serve_loop(client, config, is_master, rest)
+          {:continuation, _} ->
+            serve_loop(client, config, is_master, new_buffer)
+        end
+      {:error, :closed} ->
+        :ok
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp process_command(command, client, config, is_master) do
+    IO.puts("Received command: #{inspect(command)}")  # Debug line
+    handle_command(command, client, config, is_master)
+  end
+
+  defp handle_command(parsed_data, client, config, is_master) do
     case parsed_data do
       [command | args] ->
-        execute_command_with_config(String.upcase(to_string(command)), args, client, config)
+        execute_command_with_config(String.upcase(to_string(command)), args, client, config, is_master)
       _ ->
-        write_line("-ERR Invalid command format\r\n", client)
+        unless is_master do
+          write_line("-ERR Invalid command format\r\n", client)
+        end
     end
   end
 
@@ -322,12 +291,21 @@ require Logger
   end
   # -------------------------------------------------------------------
 
-  defp execute_command_with_config(command, args, client, config) do
+  # defp execute_command_with_config(command, args, client, config) do
+  #   case command do
+  #     "INFO" when args == ["replication"] ->
+  #       handle_info_replication(client, config)
+  #     _ ->
+  #       execute_command(command, args, client)
+  #   end
+  # end
+
+  defp execute_command_with_config(command, args, client, config, is_master) do
     case command do
       "INFO" when args == ["replication"] ->
         handle_info_replication(client, config)
       _ ->
-        execute_command(command, args, client)
+        execute_command(command, args, client, is_master)
     end
   end
 
@@ -348,7 +326,7 @@ require Logger
     write_line(packed_response, client)
   end
 
-  defp execute_command("REPLCONF", args, client) do
+  defp execute_command("REPLCONF", args, client, _is_master) do
     IO.puts("Received REPLCONF with args: #{inspect(args)}")
 
     message = "+OK\r\n"
@@ -356,7 +334,7 @@ require Logger
   end
 
 
-  defp execute_command("PSYNC", _args, client) do
+  defp execute_command("PSYNC", _args, client, _is_master) do
     try do
       response = "+FULLRESYNC #{replication_id()} #{replication_offset()}\r\n"
       :ok = :gen_tcp.send(client, response)
@@ -385,12 +363,57 @@ require Logger
   end
 
 
-  defp execute_command("ECHO", [message], client) do
+  defp execute_command("ECHO", [message], client, _is_master) do
     response = Server.Protocol.pack(message) |> IO.iodata_to_binary()
     write_line(response, client)
   end
 
-  defp execute_command("SET", [key, value | rest], client) do
+  # defp execute_command("SET", [key, value | rest], client) do
+  #   try do
+  #     case rest do
+  #       [command, time] ->
+  #         command = String.upcase(to_string(command))
+  #         if command == "PX" do
+  #           time_ms = String.to_integer(time)
+  #           Server.Store.update(key, value, time_ms)
+  #         end
+  #       [] ->
+  #         Server.Store.update(key, value)
+  #     end
+
+  #     write_line("+OK\r\n", client)
+  #     Server.Commandbuffer.add_command(["SET", key, value | rest])
+
+  #     send_buffered_commands_to_replica()
+  #     :ok
+  #   catch
+  #     _ ->
+  #       write_line("-ERR Internal server error\r\n", client)
+  #   end
+  # end
+
+  # defp execute_command("GET", [key], client) do
+  #   IO.puts("Executing GET command for key: #{key}")
+  #   case Server.Store.get_value_or_false(key) do
+  #     {:ok, value} ->
+  #       IO.puts("Value found: #{value}")
+  #       response = Server.Protocol.pack(value) |> IO.iodata_to_binary()
+  #       write_line(response, client)
+
+  #     {:error, _reason} ->
+  #       write_line("$-1\r\n", client)
+  #   end
+  # end
+
+  # defp execute_command("PING", [], client, is_master) do
+  #   write_line("+PONG\r\n", client)
+  # end
+
+  # defp execute_command(command, _args, client, is_master) do
+  #   write_line("-ERR Unknown command '#{command}'\r\n", client)
+  # end
+
+  defp execute_command("SET", [key, value | rest], client, is_master) do
     try do
       case rest do
         [command, time] ->
@@ -403,36 +426,48 @@ require Logger
           Server.Store.update(key, value)
       end
 
-      write_line("+OK\r\n", client)
+      unless is_master do
+        write_line("+OK\r\n", client)
+      end
       Server.Commandbuffer.add_command(["SET", key, value | rest])
 
       send_buffered_commands_to_replica()
       :ok
     catch
       _ ->
-        write_line("-ERR Internal server error\r\n", client)
+        unless is_master do
+          write_line("-ERR Internal server error\r\n", client)
+        end
     end
   end
 
-  defp execute_command("GET", [key], client) do
+  defp execute_command("GET", [key], client, is_master) do
     IO.puts("Executing GET command for key: #{key}")
     case Server.Store.get_value_or_false(key) do
       {:ok, value} ->
         IO.puts("Value found: #{value}")
-        response = Server.Protocol.pack(value) |> IO.iodata_to_binary()
-        write_line(response, client)
+        unless is_master do
+          response = Server.Protocol.pack(value) |> IO.iodata_to_binary()
+          write_line(response, client)
+        end
 
       {:error, _reason} ->
-        write_line("$-1\r\n", client)
+        unless is_master do
+          write_line("$-1\r\n", client)
+        end
     end
   end
 
-  defp execute_command("PING", [], client) do
-    write_line("+PONG\r\n", client)
+  defp execute_command("PING", [], client, is_master) do
+    unless is_master do
+      write_line("+PONG\r\n", client)
+    end
   end
 
-  defp execute_command(command, _args, client) do
-    write_line("-ERR Unknown command '#{command}'\r\n", client)
+  defp execute_command(command, _args, client, is_master) do
+    unless is_master do
+      write_line("-ERR Unknown command '#{command}'\r\n", client)
+    end
   end
 
   defp write_line(line, client) do
